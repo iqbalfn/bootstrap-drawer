526f582037e9d5cad58d4662a392cf154287f4d7
iqbalfn <iqbalfawz@gmail.com>
2017-01-30 12:29:14 +0700

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+    
+    grunt.initConfig({
+        concat: {
+            bs: {
+                src: [
+                    'bootstrap/js/transition.js',
+                    'bootstrap/js/alert.js',
+                    'bootstrap/js/button.js',
+                    'bootstrap/js/carousel.js',
+                    'bootstrap/js/collapse.js',
+                    'bootstrap/js/dropdown.js',
+                    'bootstrap/js/modal.js',
+                    'bootstrap/js/tooltip.js',
+                    'bootstrap/js/popover.js',
+                    'bootstrap/js/scrollspy.js',
+                    'bootstrap/js/tab.js',
+                    'bootstrap/js/affix.js',
+                    'src/js/drawer.js'
+                ],
+                dest: 'bootstrap/javascript/bootstrap.js'
+            }
+        },
+        
+        less: {
+            bs: {
+                files: {
+                    'bootstrap/css/bootstrap.css': 'bootstrap/less/bootstrap.less'
+                }
+            },
+            dist: {
+                files: {
+                    'dist/css/bootstrap-drawer.css': 'src/less/standalone.less'
+                },
+                options: {
+                    compress: true
+                }
+            }
+        },
+        
+        watch: {
+            files: ['src/js/*.js', 'src/less/*.less'],
+            tasks: ['concat:bs', 'less:bs']
+        },
+        
+        uglify: {
+            dist: {
+                files: {
+                    'dist/js/drawer.min.js': 'src/js/drawer.js'
+                },
+                options: {
+                    compress: true,
+                    report: 'gzip',
+                    preserveComments: false
+                }
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask('dist', [
+        'less:dist',
+        'uglify:dist'
+    ]);
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# bootstrap-drawer
+
+Bootstrap addons that provide drawer ( sidebar menu ) for bootstrap 3.x.x.
+
+See example and usage at the [gh-pages](http://iqbalfn.com/bootstrap-drawer).
